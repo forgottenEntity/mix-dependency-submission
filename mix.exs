c4dc99c34b63cd0c88b905a6adcb1c87d3c9adee
Jonatan Männchen <jonatan@maennchen.ch>
2025-04-23 12:45:08 +0200

    Update OTP / Elixir (#108)

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule MixDependencySubmission.MixProject do
     [
       app: :mix_dependency_submission,
       version: @version,
-      elixir: "1.17.3",
+      elixir: "1.18.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
